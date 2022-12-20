2e9d2ebe945150d6bdfbae1e54d0e0212c5e2f63
Marcin Chamera <mchamera237@gmail.com>
2022-12-20 12:17:36 +0100

    integration of Weights & Biases

---
+++ my_shared_functions.py
@@ -0,0 +1,384 @@
+from shared_functions import card_precision_top_k, prequentialSplit, EarlyStopping
+from sklearn import metrics
+import pandas as pd
+import numpy as np
+import sklearn
+import imblearn
+import torch
+import wandb
+import time
+import warnings
+warnings.filterwarnings('ignore')
+
+# this script consists of:
+# - methods and classes which were not put in shared_functions script prepared by authors of fraud detection handbook,
+#   sometimes modified or enhanced 
+
+def performance_assessment_f1_included(predictions_df, output_feature='TX_FRAUD', 
+                           prediction_feature='predictions', top_k_list=[100],
+                           rounded=True):
+    
+    AUC_ROC = metrics.roc_auc_score(predictions_df[output_feature], predictions_df[prediction_feature])
+    AP = metrics.average_precision_score(predictions_df[output_feature], predictions_df[prediction_feature])
+    F1 = metrics.f1_score(predictions_df[output_feature], predictions_df['predictions'].apply(lambda x: np.round(x)).astype(int))
+    
+    performances = pd.DataFrame([[AUC_ROC, AP, F1]], 
+                           columns=['AUC ROC','Average precision', 'F1 score'])
+    
+    for top_k in top_k_list:
+    
+        _, _, mean_card_precision_top_k = card_precision_top_k(predictions_df, top_k)
+        performances['Card Precision@'+str(top_k)]=mean_card_precision_top_k
+        
+    if rounded:
+        performances = performances.round(3)
+    
+    return performances
+
+def performance_assessment_model_collection_f1_included(fitted_models_and_predictions_dictionary, 
+                                            transactions_df, 
+                                            type_set='test',
+                                            top_k_list=[100]):
+
+    performances=pd.DataFrame() 
+    
+    for classifier_name, model_and_predictions in fitted_models_and_predictions_dictionary.items():
+    
+        predictions_df=transactions_df
+            
+        predictions_df['predictions']=model_and_predictions['predictions_'+type_set]
+        
+        performances_model=performance_assessment_f1_included(predictions_df, output_feature='TX_FRAUD', 
+                                                   prediction_feature='predictions', top_k_list=top_k_list)
+        performances_model.index=[classifier_name]
+        
+        performances=performances.append(performances_model)
+        
+    return performances
+
+def get_summary_performances_f1_included(performances_df, parameter_column_name="Parameters summary"):
+
+    metrics = ['AUC ROC','Average precision', 'F1 score', 'Card Precision@100']
+    performances_results=pd.DataFrame(columns=metrics)
+    
+    performances_df.reset_index(drop=True,inplace=True)
+
+    best_estimated_parameters = []
+    validation_performance = []
+    test_performance = []
+    
+    for metric in metrics:
+    
+        index_best_validation_performance = performances_df.index[np.argmax(performances_df[metric+' Validation'].values)]
+    
+        best_estimated_parameters.append(performances_df[parameter_column_name].iloc[index_best_validation_performance])
+        
+        validation_performance.append(
+                str(round(performances_df[metric+' Validation'].iloc[index_best_validation_performance],3))+
+                '+/-'+
+                str(round(performances_df[metric+' Validation'+' Std'].iloc[index_best_validation_performance],2))
+        )
+        
+        test_performance.append(
+                str(round(performances_df[metric+' Test'].iloc[index_best_validation_performance],3))+
+                '+/-'+
+                str(round(performances_df[metric+' Test'+' Std'].iloc[index_best_validation_performance],2))
+        )
+    
+    performances_results.loc["Best estimated parameters"]=best_estimated_parameters
+    performances_results.loc["Validation performance"]=validation_performance
+    performances_results.loc["Test performance"]=test_performance
+
+    optimal_test_performance = []
+    optimal_parameters = []
+
+    for metric in ['AUC ROC Test','Average precision Test', 'F1 score Test', 'Card Precision@100 Test']:
+    
+        index_optimal_test_performance = performances_df.index[np.argmax(performances_df[metric].values)]
+    
+        optimal_parameters.append(performances_df[parameter_column_name].iloc[index_optimal_test_performance])
+    
+        optimal_test_performance.append(
+                str(round(performances_df[metric].iloc[index_optimal_test_performance],3))+
+                '+/-'+
+                str(round(performances_df[metric+' Std'].iloc[index_optimal_test_performance],2))
+        )
+
+    performances_results.loc["Optimal parameter(s)"]=optimal_parameters
+    performances_results.loc["Optimal test performance"]=optimal_test_performance
+    
+    return performances_results
+
+def prequential_grid_search_with_sampler(transactions_df, 
+                                         classifier, sampler_list,
+                                         input_features, output_feature, 
+                                         parameters, scoring, 
+                                         start_date_training, 
+                                         n_folds=4,
+                                         expe_type='Test',
+                                         delta_train=7, 
+                                         delta_delay=7, 
+                                         delta_assessment=7,
+                                         performance_metrics_list_grid=['roc_auc'],
+                                         performance_metrics_list=['AUC ROC'],
+                                         n_jobs=-1):
+    
+    estimators = sampler_list.copy()
+    estimators.extend([('clf', classifier)])
+    
+    pipe = imblearn.pipeline.Pipeline(estimators)
+    
+    prequential_split_indices = prequentialSplit(transactions_df,
+                                                 start_date_training=start_date_training, 
+                                                 n_folds=n_folds, 
+                                                 delta_train=delta_train, 
+                                                 delta_delay=delta_delay, 
+                                                 delta_assessment=delta_assessment)
+    
+    grid_search = sklearn.model_selection.GridSearchCV(pipe, parameters, scoring=scoring, cv=prequential_split_indices, refit=False, n_jobs=n_jobs)
+    
+    X = transactions_df[input_features]
+    y = transactions_df[output_feature]
+
+    grid_search.fit(X, y)
+    
+    performances_df = pd.DataFrame()
+    
+    for i in range(len(performance_metrics_list_grid)):
+        performances_df[performance_metrics_list[i]+' '+expe_type]=grid_search.cv_results_['mean_test_'+performance_metrics_list_grid[i]]
+        performances_df[performance_metrics_list[i]+' '+expe_type+' Std']=grid_search.cv_results_['std_test_'+performance_metrics_list_grid[i]]
+
+    performances_df['Parameters'] = grid_search.cv_results_['params']
+    performances_df['Execution time'] = grid_search.cv_results_['mean_fit_time']
+    
+    return performances_df
+
+def model_selection_wrapper_with_sampler(transactions_df, 
+                                         classifier, 
+                                         sampler_list,
+                                         input_features, output_feature,
+                                         parameters, 
+                                         scoring, 
+                                         start_date_training_for_valid,
+                                         start_date_training_for_test,
+                                         n_folds=4,
+                                         delta_train=7, 
+                                         delta_delay=7, 
+                                         delta_assessment=7,
+                                         performance_metrics_list_grid=['roc_auc'],
+                                         performance_metrics_list=['AUC ROC'],
+                                         n_jobs=-1):
+
+    performances_df_validation = prequential_grid_search_with_sampler(transactions_df, classifier, sampler_list,
+                            input_features, output_feature,
+                            parameters, scoring, 
+                            start_date_training=start_date_training_for_valid,
+                            n_folds=n_folds,
+                            expe_type='Validation',
+                            delta_train=delta_train, 
+                            delta_delay=delta_delay, 
+                            delta_assessment=delta_assessment,
+                            performance_metrics_list_grid=performance_metrics_list_grid,
+                            performance_metrics_list=performance_metrics_list,
+                            n_jobs=n_jobs)
+    
+    performances_df_test = prequential_grid_search_with_sampler(transactions_df, classifier, sampler_list,
+                            input_features, output_feature,
+                            parameters, scoring, 
+                            start_date_training=start_date_training_for_test,
+                            n_folds=n_folds,
+                            expe_type='Test',
+                            delta_train=delta_train, 
+                            delta_delay=delta_delay, 
+                            delta_assessment=delta_assessment,
+                            performance_metrics_list_grid=performance_metrics_list_grid,
+                            performance_metrics_list=performance_metrics_list,
+                            n_jobs=n_jobs)
+    
+    performances_df_validation.drop(columns=['Parameters','Execution time'], inplace=True)
+    performances_df = pd.concat([performances_df_test,performances_df_validation],axis=1)
+
+    return performances_df
+
+class FraudDatasetToDevice(torch.utils.data.Dataset):
+    
+    def __init__(self, x, y, DEVICE):
+        'Initialization'
+        self.x = x
+        self.y = y
+        self.DEVICE = DEVICE
+
+    def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.x)
+
+    def __getitem__(self, index):
+        'Generates one sample of data'
+        if self.y is not None:
+            return self.x[index].to(self.DEVICE), self.y[index].to(self.DEVICE)
+        else:
+            return self.x[index].to(self.DEVICE)
+
+class SimpleFraudMLP(torch.nn.Module):
+    
+    def __init__(self, input_size, hidden_size):
+        super(SimpleFraudMLP, self).__init__()
+        self.input_size = input_size
+        self.hidden_size  = hidden_size
+        
+        self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
+        self.relu = torch.nn.ReLU()
+
+        self.fc2 = torch.nn.Linear(self.hidden_size, 1)
+        self.sigmoid = torch.nn.Sigmoid()
+        
+    def forward(self, x):
+        
+        hidden = self.fc1(x)
+        relu = self.relu(hidden)
+        output = self.fc2(relu)
+        output = self.sigmoid(output)
+        
+        return output
+
+def evaluate_model_no_grad(model,generator,criterion):
+    model.eval()
+    batch_losses = []
+    # We don't need gradients in val/test step since the parameter updates has been done in training step
+    # Using no_grad in val/test phase yields the faster inference and reduced memory usage
+    with torch.no_grad(): 
+        for x_batch, y_batch in generator:
+            # Forward pass
+            y_pred = model(x_batch)
+            # Compute Loss
+            loss = criterion(y_pred.squeeze(), y_batch)
+            batch_losses.append(loss.item())
+    mean_loss = np.mean(batch_losses)    
+    return mean_loss    
+
+def training_loop_and_saving_best_wandb(model,training_generator,valid_generator,optimizer,criterion,max_epochs=100,apply_early_stopping=True,patience=2,verbose=False, save_path='models/DL/not_named_pytorch_model.pt'):
+    model.train()
+    wandb.watch(model, criterion, log='all', log_freq=100)
+
+    if apply_early_stopping:
+        early_stopping = EarlyStopping(verbose=verbose,patience=patience)
+    
+    all_train_losses = []
+    all_valid_losses = []
+    
+    start_time=time.time()
+    for epoch in range(max_epochs):
+        model.train()
+        train_loss=[]
+        for x_batch, y_batch in training_generator:
+            optimizer.zero_grad()
+            y_pred = model(x_batch)
+            loss = criterion(y_pred.squeeze(), y_batch)
+            loss.backward()
+            optimizer.step()   
+            train_loss.append(loss.item())
+        
+        all_train_losses.append(np.mean(train_loss))
+        if verbose:
+            print('')
+            print('Epoch {}: train loss: {}'.format(epoch, np.mean(train_loss)))
+        valid_loss = evaluate_model_no_grad(model,valid_generator,criterion)
+        all_valid_losses.append(valid_loss)
+        if verbose:
+            print('valid loss: {}'.format(valid_loss))
+        wandb.log({'train loss': np.mean(train_loss), 'val loss': valid_loss}, step=epoch)
+        if apply_early_stopping:
+            if not early_stopping.continue_training(valid_loss):
+                if verbose:
+                    print("Early stopping")
+                break
+        
+    training_execution_time=time.time()-start_time
+    torch.save(model.state_dict(), save_path)
+    return model,training_execution_time,all_train_losses,all_valid_losses
+
+class FraudMLPWithEmbedding(torch.nn.Module):
+    
+        def __init__(self, categorical_inputs_modalities,numerical_inputs_size,embedding_sizes, hidden_size,p, DEVICE):
+            super(FraudMLPWithEmbedding, self).__init__()
+            self.categorical_inputs_modalities = categorical_inputs_modalities
+            self.numerical_inputs_size = numerical_inputs_size
+            self.embedding_sizes = embedding_sizes
+            self.hidden_size  = hidden_size
+            self.p = p
+            
+            assert len(categorical_inputs_modalities)==len(embedding_sizes), 'categorical_inputs_modalities and embedding_sizes must have the same length'
+            
+            #embedding layers
+            self.emb = []
+            for i in range(len(categorical_inputs_modalities)):
+                self.emb.append(torch.nn.Embedding(int(categorical_inputs_modalities[i]), int(embedding_sizes[i])).to(DEVICE))
+                
+            
+            #contenated inputs to hidden
+            self.fc1 = torch.nn.Linear(self.numerical_inputs_size+int(np.sum(embedding_sizes)), self.hidden_size)
+            self.relu = torch.nn.ReLU()
+            #hidden to output
+            self.fc2 = torch.nn.Linear(self.hidden_size, 1)
+            self.sigmoid = torch.nn.Sigmoid()
+            
+            self.dropout = torch.nn.Dropout(self.p)
+            
+        def forward(self, x):
+            #we assume that x start with numerical features then categorical features
+            inputs = [x[:,:self.numerical_inputs_size]]
+            for i in range(len(self.categorical_inputs_modalities)):
+                inputs.append(self.emb[i](x[:,self.numerical_inputs_size+i].to(torch.int64)))
+            
+            x = torch.cat(inputs,axis=1)
+            
+            
+            hidden = self.fc1(x)
+            hidden = self.relu(hidden)
+            
+            hidden = self.dropout(hidden)
+            
+            output = self.fc2(hidden)
+            output = self.sigmoid(output)
+            
+            return output
+
+def prepare_x_valid_with_categorical_features(train_df, valid_df,input_numerical_features, input_categorical_features):
+    x_valid = torch.FloatTensor(valid_df[input_numerical_features].values)
+    encoder = sklearn.preprocessing.OrdinalEncoder(handle_unknown='use_encoded_value',unknown_value=-1)
+    encoder.fit_transform(train_df[input_categorical_features].values) + 1
+    x_valid_cat = torch.IntTensor(encoder.transform(valid_df[input_categorical_features].values) + 1)
+    x_valid = torch.cat([x_valid,x_valid_cat],axis=1)
+
+    return x_valid
+
+def prepare_generators_with_categorical_features(train_df,valid_df,input_numerical_features, input_categorical_features, output_feature, DEVICE, batch_size=64):
+    x_train = torch.FloatTensor(train_df[input_numerical_features].values)
+    x_valid = torch.FloatTensor(valid_df[input_numerical_features].values)
+    y_train = torch.FloatTensor(train_df[output_feature].values)
+    y_valid = torch.FloatTensor(valid_df[output_feature].values)
+    
+    #categorical variables : encoding valid according to train
+    encoder = sklearn.preprocessing.OrdinalEncoder(handle_unknown='use_encoded_value',unknown_value=-1)
+    x_train_cat = encoder.fit_transform(train_df[input_categorical_features].values) + 1
+    categorical_inputs_modalities = np.max(x_train_cat,axis=0)+1
+    
+    x_train_cat = torch.IntTensor(x_train_cat)
+    x_valid_cat = torch.IntTensor(encoder.transform(valid_df[input_categorical_features].values) + 1)
+    
+    x_train = torch.cat([x_train,x_train_cat],axis=1)
+    x_valid = torch.cat([x_valid,x_valid_cat],axis=1)
+    
+    train_loader_params = {'batch_size': batch_size,
+              'shuffle': True,
+              'num_workers': 0}
+    valid_loader_params = {'batch_size': batch_size,
+              'num_workers': 0}
+    
+    training_set = FraudDatasetToDevice(x_train, y_train, DEVICE)
+    valid_set = FraudDatasetToDevice(x_valid, y_valid, DEVICE)
+    
+    training_generator = torch.utils.data.DataLoader(training_set, **train_loader_params)
+    valid_generator = torch.utils.data.DataLoader(valid_set, **valid_loader_params)
+    
+    return training_generator,valid_generator, categorical_inputs_modalities
---
+++ wandb_scripts.py
@@ -0,0 +1,46 @@
+import wandb
+
+# useful reusable functions for Weights & Biases
+
+def edit_hyperparameter(name, value):
+    api = wandb.Api()
+    # add 'change-required' tag to the run we want to change
+    runs = api.runs("chamera/mgr-anomaly-ts-xai", filters={'tags': 'change-required'})
+    if len(runs) == 0:
+        print('no runs found')
+    else:
+        print(f'found {len(runs)} runs')
+        for run in runs:
+            if name in run.config.keys():
+                print('hyperparameter found')
+                run.config[name] = value
+                run.update()
+
+def delete_hyperparameter(name):
+    api = wandb.Api()
+    # add 'change-required' tag to the run we want to change
+    runs = api.runs("chamera/mgr-anomaly-ts-xai", filters={'tags': 'change-required'})
+    if len(runs) == 0:
+        print('no runs found')
+    else:
+        print(f'found {len(runs)} runs')
+        for run in runs:
+            if name in run.config.keys():
+                print('hyperparameter found')
+                del run.config[name]
+                run.update()
+
+def add_hyperparameter(name, value):
+    api = wandb.Api()
+    # add 'change-required' tag to the run we want to change
+    runs = api.runs("chamera/mgr-anomaly-ts-xai", filters={'tags': 'change-required'})
+    if len(runs) == 0:
+        print('no runs found')
+    else:
+        print(f'found {len(runs)} runs')
+        for run in runs:
+            run.config[name] = value
+            run.update()
+
+if __name__ == '__main__':
+    edit_hyperparameter("max_epochs", 500)
